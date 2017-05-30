2e5d072e53add840abfc4b25eb5fb0bf2672f10e
ralex <ralex.rdz@gmail.com>
2017-05-29 20:26:09 -0500

    primera vista html y datos de prueba json

---
+++ datos.js
@@ -0,0 +1,23 @@
+let arregloDeLugares = [
+  {
+    name: 'Pepe Refacciones',
+    description: 'Se arreglan todo tipo de bicis',
+    address: 'Monterrey 215',
+    phone: '123456789',
+    schedule: 'Lun: 12:00-8:00 pm',
+    place_type: 'taller',
+    img: 'http://lorempixel.com/400/200/sports/',
+    location: {
+      lat: '19.412182',
+      lng: '-99.162828'
+    },
+    discounts: [
+      'Servicio completo a $200'
+    ],
+    rating: [2, 3, 5, 3, 2, 4, 1, 4, 2, 3, 4],
+    links: [
+      'http://www.facebook.com/peperefacciones'
+    ]
+  }
+
+]
