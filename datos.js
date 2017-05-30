2bcf5b71bdf175efadf2343ad55808444c8214dc
ralex <ralex.rdz@gmail.com>
2017-05-29 21:27:37 -0500

    primer ejemplo de DOM maniopulation}

--- datos.js
+++ datos.js
@@ -12,7 +12,10 @@ let arregloDeLugares = [
       lng: '-99.162828'
     },
     discounts: [
-      'Servicio completo a $200'
+      'Servicio completo a $200',
+      'Servicio completo a $300',
+      'Servicio completo a $500',
+      'Servicio completo a $600',
     ],
     rating: [2, 3, 5, 3, 2, 4, 1, 4, 2, 3, 4],
     links: [
@@ -21,3 +24,26 @@ let arregloDeLugares = [
   }
 
 ]
+
+function getHTMLForDiscounts (discounts) {
+  var htmlDiscounts = ''
+  console.log('discounts', discounts)
+  for (var i = 0; i < discounts.length; i++) {
+    htmlDiscounts += '<li>' + discounts[i] + '</li>'
+  }
+  return htmlDiscounts
+}
+
+document.querySelector('#place-name').innerText = arregloDeLugares[0].name
+document.querySelector('#place-type').innerText = arregloDeLugares[0].place_type
+document.querySelector('#main-img').src = arregloDeLugares[0].img
+document.querySelector('#description').innerText = arregloDeLugares[0].description
+document.querySelector('#lista-descuentos').innerHTML = getHTMLForDiscounts(arregloDeLugares[0].discounts)
+// document.querySelector('#place-name').innerText = arregloDeLugares[0].name
+// document.querySelector('#place-name').innerText = arregloDeLugares[0].name
+// document.querySelector('#place-name').innerText = arregloDeLugares[0].name
+
+
+
+
+console.log('hola')
