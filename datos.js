b1dd0924106f63095410122f7504dc9f6c02f1e4
Amelia <ecespedes@anzen.com.mx>
2017-06-07 21:07:28 -0500

    agregar lugares a datos.js

--- datos.js
+++ datos.js
@@ -44,6 +44,98 @@ let arregloDeLugares = [
     links: [
       'http://www.facebook.com/peperefacciones'
     ]
+  },
+  {
+    name: 'Vegano restaurante',
+    description: 'En la compra de una ensalada te regalamos un jugo',
+    address: 'Chiapas 888',
+    phone: '123456789',
+    schedule: 'Lun: 12:00-8:00 pm',
+    place_type: 'bike friendly',
+    img: 'http://lorempixel.com/400/200/sports/',
+    location: {
+      lat: '19.992342',
+      lng: '-99.11118'
+    },
+    discounts: [
+      'Servicio completo a $200',
+      'Servicio completo a $300',
+      'Servicio completo a $500',
+      'Servicio completo a $600',
+    ],
+    rating: [2, 3, 5, 3, 2, 4, 1, 4, 2, 3, 4],
+    links: [
+      'http://www.facebook.com/peperefacciones'
+    ]
+  },
+  {
+    name: 'Taller RemoBike',
+    description: 'Chequeo gratis',
+    address: 'Baja California 222',
+    phone: '123456789',
+    schedule: 'Lun: 12:00-8:00 pm',
+    place_type: 'taller',
+    img: 'http://lorempixel.com/400/200/sports/',
+    location: {
+      lat: '20.00001',
+      lng: '-99.24448'
+    },
+    discounts: [
+      'Servicio completo a $200',
+      'Servicio completo a $300',
+      'Servicio completo a $500',
+      'Servicio completo a $600',
+    ],
+    rating: [2, 3, 5, 3, 2, 4, 1, 4, 2, 3, 4],
+    links: [
+      'http://www.facebook.com/peperefacciones'
+    ]
+  },
+  {
+    name: 'Echale un ojo',
+    description: 'Estacionamiento para bicis público',
+    address: 'Guadalajara 231',
+    phone: '123456789',
+    schedule: 'Lun: 12:00-8:00 pm',
+    place_type: 'estacionamiento',
+    img: 'http://lorempixel.com/400/200/sports/',
+    location: {
+      lat: '19.475542',
+      lng: '-99.18628'
+    },
+    discounts: [
+      'Servicio completo a $200',
+      'Servicio completo a $300',
+      'Servicio completo a $500',
+      'Servicio completo a $600',
+    ],
+    rating: [2, 3, 5, 3, 2, 4, 1, 4, 2, 3, 4],
+    links: [
+      'http://www.facebook.com/peperefacciones'
+    ]
+  },
+  {
+    name: 'Lolitas Restaurant',
+    description: 'bicis una chela gratis',
+    address: 'Monterrey 567',
+    phone: '123456789',
+    schedule: 'Lun: 12:00-8:00 pm',
+    place_type: 'bike friendly',
+    img: 'http://lorempixel.com/400/200/sports/',
+    location: {
+      lat: '19.422211',
+      lng: '-99.29555'
+    },
+    discounts: [
+      'Servicio completo a $200',
+      'Servicio completo a $300',
+      'Servicio completo a $500',
+      'Servicio completo a $600',
+    ],
+    rating: [2, 3, 5, 3, 2, 4, 1, 4, 2, 3, 4],
+    links: [
+      'http://www.facebook.com/peperefacciones'
+    ]
   }
 ]
 
